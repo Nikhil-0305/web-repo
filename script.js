0471216eedd13e2e02ee045a145cbc708025ae84
Nikhil Bagul <npbagul370823@kkwagh.edu.in>
2025-04-03 13:35:43 +0530

    footer toogle added

---
+++ script.js
@@ -0,0 +1,12 @@
+let footer=document.querySelector('#footer')
+let n=0;
+console.log(footer);
+function change()
+{
+    n=n===0?1:0;
+    if(n){
+    footer.textContent="this is new text"}
+    else{
+        footer.textContent="this is the footer"}
+    
+}
